6ace32cc46a414b8b8010128eae5bbe76f43c43f
Max Boguslavskiy <max.bogus@gmail.com>
2021-04-11 01:40:58 +0300

    added test function to work with tests from assignment; added test function to first test

--- week1_graph_decomposition1/1_finding_exit_from_maze/reachability.py
+++ week1_graph_decomposition1/1_finding_exit_from_maze/reachability.py
@@ -1,10 +1,25 @@
-#Uses python3
+# Uses python3
 
 import sys
 
+
 def reach(adj, x, y):
-    #write your code here
+    # write your code here
     return 0
+
+
+def test(data):
+    n, m = data[0:2]
+    data = data[2:]
+    edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
+    x, y = data[2 * m:]
+    adj = [[] for _ in range(n)]
+    x, y = x - 1, y - 1
+    for (a, b) in edges:
+        adj[a - 1].append(b - 1)
+        adj[b - 1].append(a - 1)
+    return (reach(adj, x, y))
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
--- week1_graph_decomposition1/1_finding_exit_from_maze/reachability_tests.py
+++ week1_graph_decomposition1/1_finding_exit_from_maze/reachability_tests.py
@@ -1,9 +1,18 @@
 import unittest
 
+from reachability import reach, test
+
 
 class MyTestCase(unittest.TestCase):
     def test_sample_one(self):
-        self.assertEqual(True, False)
+        self.assertEqual(test([
+                             4, 4,
+                             1, 2,
+                             3, 2,
+                             4, 3,
+                             1, 4,
+                             1, 4
+                         ]), 1)
 
 
 if __name__ == '__main__':
