fb677a0d862617dd6aaf7e0c44842951e0e09a08
Max Boguslavskiy <max.bogus@gmail.com>
2021-04-11 01:57:10 +0300

    fixed formatting

--- week1_graph_decomposition1/2_adding_exits_to_maze/connected_components_tests.py
+++ week1_graph_decomposition1/2_adding_exits_to_maze/connected_components_tests.py
@@ -6,9 +6,9 @@ from connected_components import test
 class MyTestCase(unittest.TestCase):
     def test_input_one(self):
         self.assertEqual(test([
-             4,2,
-            1,2,
-            3,2
+            4, 2,
+            1, 2,
+            3, 2
         ]), 2)
 
 
