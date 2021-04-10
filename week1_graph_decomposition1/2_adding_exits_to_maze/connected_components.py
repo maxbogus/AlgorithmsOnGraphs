4dd6cb8afd1dc136fe847372f16d163982b06a9d
Max Boguslavskiy <max.bogus@gmail.com>
2021-04-11 01:56:42 +0300

    added test from assignment; added  test function

--- week1_graph_decomposition1/2_adding_exits_to_maze/connected_components.py
+++ week1_graph_decomposition1/2_adding_exits_to_maze/connected_components.py
@@ -3,10 +3,22 @@
 import sys
 
 
-def number_of_components(adj):
+def number_of_components(adj_list):
     result = 0
     #write your code here
     return result
+
+
+def test(input_data):
+    n, m = input_data[0:2]
+    data = input_data[2:]
+    edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
+    adj = [[] for _ in range(n)]
+    for (a, b) in edges:
+        adj[a - 1].append(b - 1)
+        adj[b - 1].append(a - 1)
+    return number_of_components(adj)
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
---
+++ week1_graph_decomposition1/2_adding_exits_to_maze/connected_components_tests.py
@@ -0,0 +1,16 @@
+import unittest
+
+from connected_components import test
+
+
+class MyTestCase(unittest.TestCase):
+    def test_input_one(self):
+        self.assertEqual(test([
+             4,2,
+            1,2,
+            3,2
+        ]), 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
