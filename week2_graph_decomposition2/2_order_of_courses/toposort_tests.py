5ce3a0c5222701639f22372b92e0bb59f6b632c4
Max Boguslavskiy <max.bogus@gmail.com>
2021-04-22 01:42:18 +0300

    added thtwo additional test cases

--- week2_graph_decomposition2/2_order_of_courses/toposort_tests.py
+++ week2_graph_decomposition2/2_order_of_courses/toposort_tests.py
@@ -4,7 +4,7 @@ from toposort import test
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
+    def test_sample_one(self):
         self.assertEqual(test([
             4, 3,
             1, 2,
@@ -12,6 +12,23 @@ class MyTestCase(unittest.TestCase):
             3, 1
         ]), [4, 3, 1, 2])
 
+    def test_sample_two(self):
+        self.assertEqual(test([
+            4, 1,
+            3, 1
+        ]), [2, 3, 1, 4])
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_sample_three(self):
+        self.assertEqual(test([
+            5, 7,
+            2, 1,
+            3, 2,
+            3, 1,
+            4, 3,
+            4, 1,
+            5, 2,
+            5, 3
+        ]), [5, 4, 3, 2, 1])
+
+    if __name__ == '__main__':
+        unittest.main()
