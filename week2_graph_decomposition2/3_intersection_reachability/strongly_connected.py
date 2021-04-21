3cadd2930500068589e067325390ccb4ea0a50eb
Max Boguslavskiy <max.bogus@gmail.com>
2021-04-22 01:43:36 +0300

    added test func; formatted file

--- week2_graph_decomposition2/3_intersection_reachability/strongly_connected.py
+++ week2_graph_decomposition2/3_intersection_reachability/strongly_connected.py
@@ -1,4 +1,4 @@
-#Uses python3
+# Uses python3
 
 import sys
 
@@ -7,8 +7,19 @@ sys.setrecursionlimit(200000)
 
 def number_of_strongly_connected_components(adj):
     result = 0
-    #write your code here
+    # write your code here
     return result
+
+
+def test(input_data):
+    n, m = input_data[0:2]
+    data = input_data[2:]
+    edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
+    adj = [[] for _ in range(n)]
+    for (a, b) in edges:
+        adj[a - 1].append(b - 1)
+    return number_of_strongly_connected_components(adj)
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
