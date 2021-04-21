d10e1467e16063c2eaad4d3f6421151ade307bee
Max Boguslavskiy <max.bogus@gmail.com>
2021-04-22 01:45:59 +0300

    added two assignments

--- week2_graph_decomposition2/3_intersection_reachability/strongly_connected_tests.py
+++ week2_graph_decomposition2/3_intersection_reachability/strongly_connected_tests.py
@@ -1,9 +1,29 @@
 import unittest
 
+from strongly_connected import test
+
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_sample_one(self):
+        self.assertEqual(test([
+            4, 4,
+            1, 2,
+            4, 1,
+            2, 3,
+            3, 1
+        ]), 2)
+
+    def test_sample_two(self):
+        self.assertEqual(test([
+            5, 7,
+            2, 1,
+            3, 2,
+            3, 1,
+            4, 3,
+            4, 1,
+            5, 2,
+            5, 3
+        ]), 5)
 
 
 if __name__ == '__main__':
