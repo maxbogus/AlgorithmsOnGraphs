63ab68dccc0468e218ed16d6c1cf01fcb5115711
Max Boguslavskiy <max.bogus@gmail.com>
2021-04-22 01:32:43 +0300

    added test func

--- week2_graph_decomposition2/1_cs_curriculum/acyclicity.py
+++ week2_graph_decomposition2/1_cs_curriculum/acyclicity.py
@@ -1,10 +1,21 @@
-#Uses python3
+# Uses python3
 
 import sys
 
 
 def acyclic(adj):
     return 0
+
+
+def test(input_data):
+    n, m = input_data[0:2]
+    data = input_data[2:]
+    edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
+    adj = [[] for _ in range(n)]
+    for (a, b) in edges:
+        adj[a - 1].append(b - 1)
+    return acyclic(adj)
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
