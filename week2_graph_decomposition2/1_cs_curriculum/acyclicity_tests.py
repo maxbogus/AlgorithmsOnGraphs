5b5eb931d2293a839bc45588c5a33947b6db389b
Max Boguslavskiy <max.bogus@gmail.com>
2021-04-22 01:35:50 +0300

    added two test cases for first assignment

--- week2_graph_decomposition2/1_cs_curriculum/acyclicity_tests.py
+++ week2_graph_decomposition2/1_cs_curriculum/acyclicity_tests.py
@@ -1,9 +1,29 @@
 import unittest
 
+from acyclicity import test
+
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_sample_one(self):
+        self.assertEqual(test([
+            4, 4,
+            1, 2,
+            4, 1,
+            2, 3,
+            3, 1
+        ]), 1)
+
+    def test_sample_two(self):
+        self.assertEqual(test([
+            5, 7,
+            1, 2,
+            2, 3,
+            1, 3,
+            3, 4,
+            1, 4,
+            2, 5,
+            3, 5
+        ]), 0)
 
 
 if __name__ == '__main__':
