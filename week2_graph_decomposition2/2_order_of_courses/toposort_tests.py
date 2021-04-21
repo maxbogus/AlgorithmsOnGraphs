62aaddddca1337b5a94069ace20054d8c2569056
Max Boguslavskiy <max.bogus@gmail.com>
2021-04-22 01:39:54 +0300

    added first test

--- week2_graph_decomposition2/2_order_of_courses/toposort_tests.py
+++ week2_graph_decomposition2/2_order_of_courses/toposort_tests.py
@@ -1,9 +1,16 @@
 import unittest
 
+from toposort import test
+
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, False)
+        self.assertEqual(test([
+            4, 3,
+            1, 2,
+            4, 1,
+            3, 1
+        ]), [4, 3, 1, 2])
 
 
 if __name__ == '__main__':
