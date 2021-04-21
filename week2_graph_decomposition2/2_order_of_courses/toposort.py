172a298160e303b1eebce550a73a1f3d8c958d58
Max Boguslavskiy <max.bogus@gmail.com>
2021-04-22 01:37:48 +0300

    formatted file; added test func

--- week2_graph_decomposition2/2_order_of_courses/toposort.py
+++ week2_graph_decomposition2/2_order_of_courses/toposort.py
@@ -1,17 +1,29 @@
-#Uses python3
+# Uses python3
 
 import sys
 
+
 def dfs(adj, used, order, x):
-    #write your code here
+    # write your code here
     pass
 
 
 def toposort(adj):
     used = [0] * len(adj)
     order = []
-    #write your code here
+    # write your code here
     return order
+
+
+def test(input_data):
+    n, m = input_data[0:2]
+    data = input_data[2:]
+    edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
+    adj = [[] for _ in range(n)]
+    for (a, b) in edges:
+        adj[a - 1].append(b - 1)
+    return toposort(adj)
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
@@ -25,4 +37,3 @@ if __name__ == '__main__':
     order = toposort(adj)
     for x in order:
         print(x + 1, end=' ')
-
