40c06defa03b255faf35850b58238ed0fa622090
Max Boguslavskiy <max.bogus@gmail.com>
2021-05-15 02:10:19 +0300

    added draft test file; created test function

--- week3_paths_in_graphs1/1_flight_segments/bfs.py
+++ week3_paths_in_graphs1/1_flight_segments/bfs.py
@@ -7,6 +7,19 @@ def distance(adj, s, t):
     #write your code here
     return -1
 
+
+def test(test_data):
+    n, m = test_data[0:2]
+    test_data = test_data[2:]
+    graph_edges = list(zip(test_data[0:(2 * m):2], test_data[1:(2 * m):2]))
+    adj_list = [[] for _ in range(n)]
+    for (a, b) in graph_edges:
+        adj_list[a - 1].append(b - 1)
+        adj_list[b - 1].append(a - 1)
+    s, t = test_data[2 * m] - 1, test_data[2 * m + 1] - 1
+    return distance(adj_list, s, t)
+
+
 if __name__ == '__main__':
     input = sys.stdin.read()
     data = list(map(int, input.split()))
