b05560e14c619765fbb24d871023018b0ffe8661
Max Boguslavskiy <max.bogus@gmail.com>
2021-05-15 02:13:23 +0300

    added two tests

--- week3_paths_in_graphs1/1_flight_segments/bfs_tests.py
+++ week3_paths_in_graphs1/1_flight_segments/bfs_tests.py
@@ -1,9 +1,28 @@
 import unittest
 
+from bfs import test
+
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_first_sample(self):
+        self.assertEqual(test([
+            4, 4,
+            1, 2,
+            4, 1,
+            2, 3,
+            3, 1,
+            2, 4
+        ]), 2)
+
+    def test_second_sample(self):
+        self.assertEqual(test([
+            5,4,
+            5,2,
+            1,3,
+            3,4,
+            1, 4,
+            3, 5
+        ]), -1)
 
 
 if __name__ == '__main__':
