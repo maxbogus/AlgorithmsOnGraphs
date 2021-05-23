821cf9e89a413d33c2cc061fdc5e4f7f18aea359
Max Boguslavskiy <max.bogus@gmail.com>
2021-05-24 01:58:07 +0300

    added test func

--- week4_paths_in_graphs2/3_exchanging_money/shortest_paths.py
+++ week4_paths_in_graphs2/3_exchanging_money/shortest_paths.py
@@ -9,6 +9,33 @@ def shortet_paths(adj, cost, s, distance, reachable, shortest):
     pass
 
 
+def test_function(test_data):
+    n, m = test_data[0:2]
+    data = test_data[2:]
+    edges = list(zip(zip(data[0:(3 * m):3], data[1:(3 * m):3]), data[2:(3 * m):3]))
+    data = data[3 * m:]
+    adj = [[] for _ in range(n)]
+    cost = [[] for _ in range(n)]
+    for ((a, b), w) in edges:
+        adj[a - 1].append(b - 1)
+        cost[a - 1].append(w)
+    s = data[0]
+    s -= 1
+    distance = [10 ** 19] * n
+    reachable = [0] * n
+    shortest = [1] * n
+    shortet_paths(adj, cost, s, distance, reachable, shortest)
+    result = []
+    for x in range(n):
+        if reachable[x] == 0:
+            result.append('*')
+        elif shortest[x] == 0:
+            result.append('-')
+        else:
+            result.append(distance[x])
+    return result
+
+
 if __name__ == '__main__':
     input = sys.stdin.read()
     data = list(map(int, input.split()))
