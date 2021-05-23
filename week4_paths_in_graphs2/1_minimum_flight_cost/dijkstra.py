c662cde3dca35628350412223b6355c6a5219078
Max Boguslavskiy <max.bogus@gmail.com>
2021-05-23 03:46:15 +0300

    added test func

--- week4_paths_in_graphs2/1_minimum_flight_cost/dijkstra.py
+++ week4_paths_in_graphs2/1_minimum_flight_cost/dijkstra.py
@@ -9,6 +9,20 @@ def distance(adj, cost, s, t):
     return -1
 
 
+def test_function(input_data):
+    n, m = input_data[0:2]
+    data = input_data[2:]
+    edges = list(zip(zip(data[0:(3 * m):3], data[1:(3 * m):3]), data[2:(3 * m):3]))
+    data = data[3 * m:]
+    adj = [[] for _ in range(n)]
+    cost = [[] for _ in range(n)]
+    for ((a, b), w) in edges:
+        adj[a - 1].append(b - 1)
+        cost[a - 1].append(w)
+    s, t = data[0] - 1, data[1] - 1
+    return distance(adj, cost, s, t)
+
+
 if __name__ == '__main__':
     input = sys.stdin.read()
     data = list(map(int, input.split()))
